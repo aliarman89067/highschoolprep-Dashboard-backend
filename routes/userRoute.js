deb3af7d6a27a70e0acd383981bd34436a85b5a1
Ali Arman <ali@gmail.com>
2024-11-13 10:11:10 -0800

    Update cookir

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -378,14 +378,14 @@ router.get("/logout-user", async (req, res) => {
   try {
     const token = req.cookies.highschoolprepadminId;
     if (!token) {
-      res.cookie("highschoolprepadminId", "");
+      res.clearCookie("highschoolprepadminId");
       return res.status(404).json({
         success: false,
         message: "Token not found but still removed",
       });
     }
 
-    res.cookie("highschoolprepadminId", "");
+    res.clearCookie("highschoolprepadminId");
     res.status(200).json({ success: true, message: "Token was deleted" });
   } catch (error) {
     console.log(error);
